c8b5e5fae2486058d77fbcc0be54fe9388556f21
DarshiniKurasa <abhipriya.kurasa@gmail.com>
2025-04-03 16:24:07 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: "https://codementor-ai-frontend.onrender.com",  // Allow only your frontend
+  methods: "GET,POST",
+}));
 app.use(express.json())
 
 app.get('/', (req, res) => {
